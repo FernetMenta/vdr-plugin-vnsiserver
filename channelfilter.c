2a694330d824f53142b1aa2a14a0f6fb3f102b74
AlwinEsch <alwin.esch@web.de>
2015-04-06 18:38:37 +0200

    Rework all Licences and change to KODI

--- channelfilter.c
+++ channelfilter.c
@@ -1,6 +1,10 @@
 /*
+ *      vdr-plugin-vnsi - KODI server plugin for VDR
+ *
  *      Copyright (C) 2005-2013 Team XBMC
- *      http://xbmc.org
+ *      Copyright (C) 2015 Team KODI
+ *
+ *      http://kodi.tv
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -13,7 +17,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, see
+ *  along with KODI; see the file COPYING.  If not, see
  *  <http://www.gnu.org/licenses/>.
  *
  */
