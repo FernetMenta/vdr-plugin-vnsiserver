120fb57371c29ecae7d753286cec2e8779a85fa6
Max Kellermann <max@duempel.org>
2015-11-07 19:05:23 +0100

    requestpacket: add range checks to fix buffer overflow
    Calling strlen() on a buffer without knowing if there's a null
    terminator is easy to crash.  The strlen() function will simply run
    across the buffer end, until it happens to find a null byte in the
    following (undefined) data section, or until it gets killed by the
    kernel.
    
    Another problem is that the method serverError() does not work.  It
    works only if nobody has moved the packetPos yet - which is a wrong
    assumption to make.

--- requestpacket.c
+++ requestpacket.c
@@ -72,8 +72,13 @@ char* cRequestPacket::extract_String()
 {
   if (serverError()) return NULL;
 
-  int length = strlen((char*)&userData[packetPos]);
-  if ((packetPos + length) > userDataLength) return NULL;
+  const char *p = (const char *)&userData[packetPos];
+  const char *end = (const char *)memchr(p, '\0', userDataLength - packetPos);
+  if (end == NULL)
+    /* string is not terminated - fail */
+    return NULL;
+
+  int length = end - p;
   char* str = new char[length + 1];
   strcpy(str, (char*)&userData[packetPos]);
   packetPos += length + 1;
